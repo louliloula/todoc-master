413e567294eed15b5e719e6a1fb2ee426e56d862
loula <wmhadjou@gmail.com>
2022-01-12 13:41:34 +0100

    Ajout DAO

---
+++ app/src/main/java/com/cleanup/todoc/database/dao/ProjectDao.java
@@ -0,0 +1,7 @@
+package com.cleanup.todoc.database.dao;
+
+import androidx.room.Dao;
+
+@Dao
+public interface ProjectDao {
+}
---
+++ app/src/main/java/com/cleanup/todoc/database/dao/TaskDao.java
@@ -0,0 +1,8 @@
+package com.cleanup.todoc.database.dao;
+
+import androidx.room.Dao;
+
+@Dao
+public interface TaskDao {
+
+}
